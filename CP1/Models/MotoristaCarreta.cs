17615189a4edae3b387f9ef62b3a0fefab2c0886
witalonrodrigues <witalonar@outlook.com>
2026-09-10 21:39:33 -0300

    feat: cria classe filha MotoristaCarreta

---
+++ CP1/Models/MotoristaCarreta.cs
@@ -0,0 +1,25 @@
+﻿using CP1.Models;
+using System;
+
+namespace CP1.Models
+{
+    public class MotoristaCarreta : FuncionarioTransporte
+    {
+        public string PlacaVeiculo { get; private set; }
+        public string CategoriaCNH { get; private set; }
+
+        public MotoristaCarreta(string nome, string registro, string placaVeiculo, string categoriaCnh)
+            : base(nome, registro)
+        {
+            PlacaVeiculo = placaVeiculo;
+            CategoriaCNH = categoriaCnh;
+        }
+
+        public override void MostrarDetalhes()
+        {
+            Console.WriteLine("--- Motorista de Carreta ---");
+            base.MostrarDetalhes();
+            Console.WriteLine($"Placa: {PlacaVeiculo} | Categoria CNH: {CategoriaCNH}\n");
+        }
+    }
+}
