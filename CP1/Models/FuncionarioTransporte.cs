83b3b6ce172244c82ff510f608c978f484eb4f3d
witalonrodrigues <witalonar@outlook.com>
2026-09-10 21:38:11 -0300

    feat: cria classe abstrata FuncionarioTransporte

---
+++ CP1/Models/FuncionarioTransporte.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CP1.Models
+{
+    public abstract class FuncionarioTransporte
+    {
+        public string Nome { get; private set; }
+        public string Registro { get; private set; }
+
+        public FuncionarioTransporte(string nome, string registro)
+        {
+            Nome = nome;
+            Registro = registro;
+        }
+
+        public virtual void MostrarDetalhes()
+        {
+            Console.WriteLine($"Nome: {Nome} | Registro: {Registro}");
+        }
+    }
+}
