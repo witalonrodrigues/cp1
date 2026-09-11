b7fd25787d0fd129466615bd3b06d4c68b0f3d23
witalonrodrigues <witalonar@outlook.com>
2026-09-10 21:47:33 -0300

    feat: implementa instâncias e polimorfismo no console

--- CP1/Program.cs
+++ CP1/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using CP1.Models;
+using System;
+
+namespace CP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sistema De Logística para Transportadora\n");
+
+            MotoristaCarreta motorista = new MotoristaCarreta("Alexandre", "REG001", "ABC-1234", "E");
+            EntregadorMoto entregador = new EntregadorMoto("Enzo", "REG002", "XYZ-9876", true);
+
+            FuncionarioTransporte[] frota = new FuncionarioTransporte[]
+            {
+                motorista,
+                entregador
+            };
+
+            foreach (FuncionarioTransporte funcionario in frota)
+            {
+                funcionario.MostrarDetalhes();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
