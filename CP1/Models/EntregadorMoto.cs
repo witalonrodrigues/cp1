109651ce7d5f08eb66a292c60ef84b163fe03e17
witalonrodrigues <witalonar@outlook.com>
2026-09-10 21:44:34 -0300

    feat: cria classe filha EntregadorMoto

---
+++ CP1/Models/EntregadorMoto.cs
@@ -0,0 +1,25 @@
+﻿using CP1.Models;
+using System;
+
+namespace CP1.Models
+{
+    public class EntregadorMoto : FuncionarioTransporte
+    {
+        public string PlacaMoto { get; private set; }
+        public bool TemBau { get; private set; }
+
+        public EntregadorMoto(string nome, string registro, string placaMoto, bool temBau)
+            : base(nome, registro)
+        {
+            PlacaMoto = placaMoto;
+            TemBau = temBau;
+        }
+
+        public override void MostrarDetalhes()
+        {
+            Console.WriteLine("--- Entregador de Moto ---");
+            base.MostrarDetalhes();
+            Console.WriteLine($"Placa: {PlacaMoto} | Possui Baú: {(TemBau ? "Sim" : "Não")}\n");
+        }
+    }
+}
